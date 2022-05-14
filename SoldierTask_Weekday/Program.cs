19dce6b9ad6d8f63bd616125f6c921937c7126c5
Malik8008 <tu60prbs1@code.edu.az>
2022-05-14 13:39:36 +0400

    first commit

---
+++ SoldierTask_Weekday/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace SoldierTask_Weekday
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+            do
+            {
+                int capaCapacity = Convert.ToInt32(Console.ReadLine());
+                int Count = Convert.ToInt32(Console.ReadLine());
+                bool IsAuto = Convert.ToBoolean(Console.ReadLine());
+
+                Weapon weapon = new Weapon(capaCapacity, Count, IsAuto);
+
+
+                Console.WriteLine("0   informasya almaq");
+                Console.WriteLine("1   shoot method");
+                Console.WriteLine("2   Fire method");
+                Console.WriteLine("3   GetRaminBulletCount");
+                Console.WriteLine("4   Relod method");
+                Console.WriteLine("5   ChangeFireMode");
+                Console.WriteLine("6   exit");
+                int check = Convert.ToInt32(Console.ReadLine());
+
+                switch (check)
+                {
+                    case 1:
+                        weapon.Shoot();
+                        break;
+                    case 2:
+                        weapon.Fire();
+                        break;
+                    case 3:
+                        weapon.GetRemainBullentCount();
+                        break;
+                    case 4:
+                        weapon.Reload();
+                        break;
+                    case 5:
+                        weapon.ChangeFireMode();
+                        break;
+                    case 6:
+                        
+                        break;
+                    default:
+                        break;
+
+                }
+
+            } while (true);
+
+
+
+        }
+    }
+}
---
+++ SoldierTask_Weekday/Weapon.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SoldierTask_Weekday
+{
+    internal class Weapon
+    {
+        public static int Capacity { get; set; }
+        public static int Count { get; set; }
+        public static bool IsAuto { get; set; }
+
+
+        public Weapon(int capacity, int count, bool isauto)
+        {
+            Capacity = capacity;
+            Count = count;
+            IsAuto = isauto;
+        }
+
+
+
+        public void Shoot()
+        {
+            if (IsAuto == false)
+            {
+                Count = Count - 1;
+            }
+            Console.WriteLine(Count);
+
+        }
+
+        public void Fire()
+        {
+            if (IsAuto == true)
+            {
+                for (int i = 0; i < Count; i--)
+                {
+                    if (Count > 0)
+                    {
+                        Count--;
+                        Console.WriteLine(Count);
+                    }
+                }
+            }
+        }
+
+
+        public int GetRemainBullentCount()
+        {
+            return Capacity - Count;
+        }
+
+
+        public void Reload()
+        {
+            Console.WriteLine(Capacity = Count + GetRemainBullentCount());
+        }
+
+
+        public void ChangeFireMode()
+        {
+            if (IsAuto == false)
+            {
+                IsAuto = true;
+            }
+            else if (IsAuto == true)
+            {
+                IsAuto = false;
+            }
+        }
+
+       
+
+
+    }
+}
